2f073d451b45e0ec4639825d693753eec0e50c0e
aviv-francoislaforgia <francois.laforgia@aviv-group.com>
2023-04-24 15:22:06 +0200

    Add files via upload

--- gitPullManager.sh
+++ gitPullManager.sh
@@ -3,13 +3,10 @@
 x=0
 while [ $x -le 10 ]
 do
-  OUTPUT=$(cd /Users/francois.laforgia/Documents/dev/dh/DataHub/TestGlossaries ; git pull ; cd ../..)  
-  #echo $OUTPUT
-  if [ "$OUTPUT" = 'Already up to date.' ]
-   then
-     echo "no changes"
-   else
-     echo $OUTPUT
+  OUTPUT=$(git pull)  
+  if [ "$OUTPUT" != "Already up to date." ]
+  then
+    echo $OUTPUT
   fi
   let x++
   sleep 5
